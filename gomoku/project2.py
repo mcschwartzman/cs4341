61634e130a4a3837f8bc27bf76486c2dd2a1ee5b
Mathew Schwartzman <mathew.s.7196@outlook.com>
2017-09-15 14:36:55 -0400

    first referee function

--- gomoku/project2.py
+++ gomoku/project2.py
@@ -1,11 +1,20 @@
+#Created 14 September 2017
+
 import os
 
+
+###Boolean Function to check if My Turn###
 def isMyTurn():
 
-	#fileExistsBool = 
+  fileExistsBool = os.path.isfile('./file.txt')
 
-	if (os.path.isfile('./file.txt')):	# if groupname.txt exists
-		print "my turn"	# it is my turn
-	else:
-		print "not my turn"	# it is not my turn
+  if (fileExistsBool):  # if groupname.txt exists
+    print "my turn" # it is my turn
+  else:
+    print "not my turn" # it is not my turn
 
+  print fileExistsBool
+  return fileExistsBool
+
+
+isMyTurn()
