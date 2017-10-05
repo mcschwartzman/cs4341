00fd0de888f7f0b5bcfd49c94ea87fdbe7f54e26
Mathew Schwartzman <mathew.s.7196@outlook.com>
2017-10-05 14:36:04 -0400

    experimenting

--- Project3/project3.py
+++ Project3/project3.py
@@ -7,3 +7,48 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.utils import np_utils
+
+
+
+nb_classes = 10
+
+# the data, shuffled and split between tran and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print("X_train original shape", X_train.shape)
+print("y_train original shape", y_train.shape)
+
+
+
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_train[i], cmap='gray', interpolation='none')
+    plt.title("Class {}".format(y_train[i]))
+
+
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print("Training matrix shape", X_train.shape)
+print("Testing matrix shape", X_test.shape)
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+model.add(Dense(512, input_shape=(784,)))
+model.add(Activation('relu')) # An "activation" is just a non-linear function applied to the output
+                              # of the layer above. Here, with a "rectified linear unit",
+                              # we clamp all values below 0 to 0.
+                           
+model.add(Dropout(0.2))   # Dropout helps protect the model from memorizing or "overfitting" the training data
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
+model.add(Activation('softmax')) # This special "softmax" activation among other things,
+                                 # ensures the output is a valid probaility distribution, that is
+                                 # that its values are all non-negative and sum to 1.
