e7b4b257e10e6846144154b20fe451cc65622266
Mathew Schwartzman <mathew.s.7196@outlook.com>
2017-10-05 16:25:32 -0400

    started on classes

--- Project3/neuralnetwork.py
+++ Project3/neuralnetwork.py
@@ -6,9 +6,25 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 import numpy
 
-data = numpy.load('images.npy')
+class zero:
+	def __init__(self, label, image):
+		self.label = label
+		self.image = image # use function to find image per label?
 
-(x_train, y_train), (x_val, y_val) = data
+##... use different class for each number ...##
+
+
+
+
+labelarray = numpy.load('labels.npy')
+
+######=========== Read data into chunks for validation, test, and train ========########
+
+print(data)
+
+for n in data:
+	print(n)
+
 
 
 
@@ -17,6 +33,7 @@ model = Sequential()
 model.add(Dense(10, input_shape = (28*28, ), kernel_initializer='he_normal'))
 model.add(Activation('relu'))
 
+#############============ MODEL GOES HERE ====================############
 
 model.add(Dense(10, kernel_initializer='he_normal'))
 model.add(Activation('softmax'))
